07fcb471017d0d81406773a447b67c52fab05381
Bryan Taboada <b.taboada123@gmail.com>
2025-01-22 17:53:01 -0700

    added new resume

--- src/pages/resumePage.jsx
+++ src/pages/resumePage.jsx
@@ -2,12 +2,12 @@ export default function ResumePage() {
   return (
     <section className="text-center">
       <h1 className=" p-4">Resume</h1>
-        <img className="shadow-lg img-fluid rounded" src="./assets/images/ResumeImage.jpg" alt="Resume" />
+        <img className="shadow-lg img-fluid rounded" src="./assets/images/Taboada2025Resume.jpg" alt="Resume" />
       <div className=" pb-4">
         <p className="pt-3">View or download my resume below:</p>
         {/* Download Link */}
         <a 
-           href="./assets/resume/ResumeUpdate.pdf" 
+           href="./assets/resume/Taboada2025Resume.pdf" 
           target="_blank" 
           rel="noopener noreferrer"
           className="btn btn-primary"
